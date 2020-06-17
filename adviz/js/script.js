c714812b0bc400e55c7e722dd3591bb2ab06d51e
ImCry <y.samet210@gmail.com>
2020-06-17 23:18:02 +0200

    add user and add new marker works Co-authored-by: sami-htw <community4coding@gmail.com>

--- adviz/js/script.js
+++ adviz/js/script.js
@@ -1,21 +1,24 @@
+let map = null;
+let featureGroup = null;
 function showMap(list) {
-
   L.mapquest.key = 'CKvWSTNixxAb9dVV0kMTryinWJdAqS8K';
-
   // Geocode three locations, then call the createMap callback
   L.mapquest.geocoding().geocode(list, createMap);
-
   // 'map' refers to a <div> element with the ID map
   function createMap(error, response) {
     // Initialize the Map
-    let map = L.mapquest.map('map', {
-      layers: L.mapquest.tileLayer('map'),
-      center: [0, 0],
-      zoom: 12
-    });
-
+    if(map == null){
+        map = L.mapquest.map('map', {
+        layers: L.mapquest.tileLayer('map'),
+        center: [0, 0],
+        zoom: 14
+      });
+    }else{
+      map.removeLayer(featureGroup);
+    }
+    
     // Generate the feature group containing markers from the geocoded locations
-    let featureGroup = generateMarkersFeatureGroup(response);
+    featureGroup = generateMarkersFeatureGroup(response);
 
     // Add markers to the map and zoom to the features
     featureGroup.addTo(map);
@@ -49,6 +52,10 @@ function showMap(list) {
   }
 }
 
+
+
+
+
 const login_button = document.querySelector('#login-button')
 //const loginForm = document.querySelector('#login-form');
 const login_userName = document.querySelector('#username');
@@ -61,50 +68,92 @@ login_button.addEventListener('click', function (event) {
   }).indexOf(login_userName.value);
   if (currentUser !== -1 && users[currentUser]["pass"] === login_password.value) {
     managePermission();
+    loadContactsLocalStorage();
     openMaps();
+    showMap(getAdressList());
   } else {
     event.preventDefault();
     alert("Wrong Username or password")
   }
 });
 
+
+
 function getAdressList() {
   let list = [];
   let del = ', ';
+  let adress = '';
   for (i = 0; i < contacts.length; i++) {
     adress = contacts[i]['street'] + del + contacts[i]['plz'] + del + contacts[i]['city'] + del + contacts[i]['country'];
     list.push(adress);
+
+
   }
+
+
   return list;
 }
+
 
 function getAdressListdisplay() {
   let list = [];
   let del = ', ';
-  for (i = 0; i < contacts.length; i++) {
-    adress = contacts[i]['firstName'] + del + contacts[i]['lastName'] + del + contacts[i]['street'] + del + contacts[i]['plz'] + del + contacts[i]['city'] + del + contacts[i]['country'];
-    list.push(adress);
+  let i, checkValue;
+  let adress = '';
+
+
+  /*
+   * if normalo skip all private users, else create list of all
+   */
+  if (users[currentUser]['permission'] === "normal") {
+    for (i = 0; i < contacts.length; i++) {
+      if (contacts[i]['priv']=== true){
+        continue;
+      }
+      adress = 'Vorname :' + contacts[i]['firstName'] + del + 'Nachname :' + contacts[i]['lastName'] + del + 'Straße :' + contacts[i]['street'] + del + 'Postleitzahl:' + contacts[i]['plz'] + del + 'Stadt :' + contacts[i]['city'] + del + 'Land :' + contacts[i]['country'];
+      list.push(adress);
+    }
+  }else{
+    for (i = 0; i < contacts.length; i++) {
+      adress = 'Vorname :' + contacts[i]['firstName'] + del + 'Nachname :' + contacts[i]['lastName'] + del + 'Straße :' + contacts[i]['street'] + del + 'Postleitzahl:' + contacts[i]['plz'] + del + 'Stadt :' + contacts[i]['city'] + del + 'Land :' + contacts[i]['country'];
+      list.push(adress);
+  
+    }
   }
-  return list;
+
+  
+  return list; // hinter den Aktuallisierung wird list zurück gegeben ,und drin stehen alle users
 }
 
+
+
+
 function openMaps() {
+  let element ;
+  let contactList = document.querySelector('#fieldMembers > ul');
+  if (contactList != null) {
+    contactList.remove();
+  }
+    
   location.href = '#maps';
   ul = document.createElement('ul');
 
-  document.getElementById('myItemList').appendChild(ul);
+  document.querySelector('#fieldMembers').appendChild(ul);
 
   getAdressListdisplay().forEach(function (item) {
     let li = document.createElement('li');
     ul.appendChild(li);
+    li.addEventListener('click', updateContact); //TODO idea
+
+
+
 
     li.innerHTML += item;
   });
-  showMap(getAdressList());
 }
 
 function managePermission() {
-  if (users[currentUser]["permission"] === "normal") {
+  if (users[currentUser]['permission'] === "normal") {
     document.querySelector("#maps-add-button").style.display = "none";
     document.querySelector("#maps-update-button").style.display = "none";
     document.querySelector("#update-update-button").style.display = "none";
@@ -112,10 +161,6 @@ function managePermission() {
   }
 }
 
-
-function addUser(){
-
-}
 
 var L;
 
@@ -138,7 +183,7 @@ let contacts = [{
   plz: "10555",
   city: "Berlin",
   country: "Deutschland",
-  private: false
+  priv: false
 }, {
   firstName: "Max",
   lastName: "Müller",
@@ -146,5 +191,58 @@ let contacts = [{
   plz: "10178",
   city: "Berlin",
   country: "Deutschland",
-  private: true
+  priv: true
 }];
+
+
+const insert_button = document.querySelector('#btn-insert');
+
+insert_button.addEventListener('click', addContact);
+
+
+function addContact(){
+  let addForm = document.querySelector('#add-form');
+  let firstName = document.querySelector("#add-first_name").value;
+  let lastName = document.querySelector("#add-last_name").value;
+  let strasse = document.querySelector("#add-street").value;
+  let plz = document.querySelector("#add-plz").value;
+  let stadt = document.querySelector("#add-city").value;
+  let land = document.querySelector("#add-country").value;
+  let priv = document.querySelector("#add-priv").checked;
+
+  let newContact = {
+
+    firstName: firstName,
+    lastName: lastName,
+    street: strasse,
+    plz: plz,
+    city: stadt,
+    country: land,
+    priv: priv
+  
+  }
+
+  contacts.push(newContact);
+  addForm.reset();
+  saveContactsLocalStorage();
+  openMaps();
+  showMap(getAdressList());
+}
+
+function updateContact(){
+  console.log("Hello World");
+}
+
+
+
+
+function saveContactsLocalStorage(){
+  localStorage.setItem('contacts', JSON.stringify(contacts));
+}
+
+function loadContactsLocalStorage(){
+  if(localStorage.getItem('contacts') != null){
+    contacts = JSON.parse(localStorage.getItem('contacts'));
+  }
+}
+
